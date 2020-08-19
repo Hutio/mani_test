f1b78689ca0081430018df87634efe1daedf9963
Hutio <derecho11@naver.com>
2020-08-18 18:10:55 -0700

    added class

--- scripts/mani_subscriber.py
+++ scripts/mani_subscriber.py
@@ -8,22 +8,26 @@ from dynamixel_sdk import *
 
 #--------------------------------Settings-------------------------------------#
 
-# Motor settings
-ADDR_AX_TORQUE_ENABLE      = 24
-ADDR_AX_MOVING_SPEED       = 32
-ADDR_AX_PRESENT_SPEED      = 38
-ADDR_AX_LIGHT              = 25
-
 PROTOCOL_VERSION           = 1.0
 # Default setting
 BAUDRATE                   = 1000000
 DEVICENAME                 = '/dev/ttyUSB0'
 LEN_MOTOR_SCAN             = 4
 
+
+
+#--------------------------------Don't touch----------------------------------#
+# Motor Address
+ADDR_AX_TORQUE_ENABLE      = 24
+ADDR_AX_MOVING_SPEED       = 32
+ADDR_AX_PRESENT_SPEED      = 38
+ADDR_AX_LIGHT              = 25
+
+COMM_SUCCESS               = 0
+
 DXL_ENABLE                 = 1
 DXL_DISABLE                = 0
 
-#--------------------------------Don't touch----------------------------------#
 connected_motor = []
 portHandler = PortHandler(DEVICENAME)
 packetHandler = PacketHandler(PROTOCOL_VERSION)
@@ -48,39 +52,11 @@ def Dynamixel_Set_baudrate():
         getch()
         quit()
 
-def Dynamixel_Torque_enable(DXL_ID):
-    dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_AX_TORQUE_ENABLE, DXL_ENABLE)
-    if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-    elif dxl_error != 0:
-        print("%s" % packetHandler.getRxPacketError(dxl_error))
-    else:
-        print("Dynamixel has been successfully connected")
-        Dynamixel_Light_enable(DXL_ID)
-
-def Dynamixel_Torque_disable(DXL_ID):
-    dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_AX_TORQUE_ENABLE, DXL_DISABLE)
-    if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-    elif dxl_error != 0:
-        print("%s" % packetHandler.getRxPacketError(dxl_error))
-    else:
-        Dynamixel_Light_disable(DXL_ID)
-
 def Dynamixel_Light_enable(DXL_ID):
     dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_AX_LIGHT, DXL_ENABLE)
 
 def Dynamixel_Light_disable(DXL_ID):
     dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_AX_LIGHT, DXL_DISABLE)
-
-def Dynamixel_Write_motor(DXL_ID, spd, sec):
-    dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_AX_MOVING_SPEED, spd)
-    if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-    elif dxl_error != 0:
-        print("%s" % packetHandler.getRxPacketError(dxl_error))
-    time.sleep(sec)
-    dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_AX_MOVING_SPEED, DXL_DISABLE)
 
 def Dynamixel_Read_motor(DXL_ID):
     dxl_present_speed, dxl_comm_result, dxl_error = packetHandler.read2ByteTxRx(portHandler, DXL_ID, ADDR_AX_PRESENT_SPEED)
@@ -101,6 +77,48 @@ def Dynamixel_Ping(DXL_ID):
         print("[ID:%03d] ping Succeeded." % DXL_ID)
         connected_motor.append(DXL_ID)
 
+class Dynamixel_Motor_control:
+
+    def __init__(self,connected_motor):
+        A = 0
+
+    def Write_motor(DXL_ID, spd, sec):
+        dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_AX_MOVING_SPEED, spd)
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % packetHandler.getRxPacketError(dxl_error))
+        time.sleep(sec)
+        dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_AX_MOVING_SPEED, DXL_DISABLE)
+
+    def Torque_enable(DXL_ID):
+        dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_AX_TORQUE_ENABLE, DXL_ENABLE)
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % packetHandler.getRxPacketError(dxl_error))
+        else:
+            Dynamixel_Light_enable(DXL_ID)
+
+    def Torque_disable(DXL_ID):
+        dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_AX_TORQUE_ENABLE, DXL_DISABLE)
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % packetHandler.getRxPacketError(dxl_error))
+        else:
+            Dynamixel_Light_disable(DXL_ID)
+
+    def Dynamixel_Motor_enable():
+        for i in range(LEN_MOTOR_SCAN):
+            Dynamixel_Ping(i)
+            Torque_enable(i)
+        print("Motor [" + ','.join(map(str,connected_motor)) + "] successfully connected")
+
+    def Dynamixel_Motor_disable():
+        for i in range(len(connected_motor)):
+            Torque_disable(i)
+
 def Dynamixel_Close_port():
     portHandler.closePort()
 
@@ -117,29 +135,21 @@ def listener():
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
-def Dynamixel_Motor_enable():
-    for i in range(LEN_MOTOR_SCAN):
-        Dynamixel_Ping(i)
-        Dynamixel_Torque_enable(i)
-    print("Motor [" + ','.join(map(str,connected_motor)) + "] successfully connected")
-
-def Dynamixel_Motor_disable():
-    for i in range(len(connected_motor)):
-        Dynamixel_Torque_disable(i)
 
 if __name__ == '__main__':
     Dynamixel_Open_port()
     Dynamixel_Set_baudrate()
 
-    Dynamixel_Motor_enable()
-#    listener()
-    Dynamixel_Write_motor(0,500,3)
-    Dynamixel_Write_motor(1,1524,3)
-    Dynamixel_Write_motor(2,500,3)
-    Dynamixel_Write_motor(3,1524,3)
+    dmc = Dynamixel_Motor_control(connected_motor)
 
-    Dynamixel_Motor_disable()
+    dmc.Motor_enable()
+#   listener()
+    dmc.Write_motor(0,500,3)
+    dmc.Write_motor(1,1524,3)
+    dmc.Write_motor(2,500,3)
+    dmc.Write_motor(3,1524,3)
+
+    dmc.Motor_disable()
 
     Dynamixel_Close_port()
     print("Successfully Node Closed")
-
