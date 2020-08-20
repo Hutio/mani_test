84eec56c1e2cb99211764112614c70509978ef76
Hutio <derecho11@naver.com>
2020-08-20 01:27:13 -0700

    fix

--- scripts/mani_publisher.py
+++ scripts/mani_publisher.py
@@ -1,37 +1,7 @@
 #!/usr/bin/env python2
-# Software License Agreement (BSD License)
+# 
 #
-# Copyright (c) 2008, Willow Garage, Inc.
-# All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Revision $Id$
 
 ## Simple talker demo that published std_msgs/Strings messages
 ## to the 'chatter' topic
@@ -44,12 +14,12 @@ msg = Mdata()
 def talker():
     pub = rospy.Publisher("Motor_data", Mdata, queue_size=1)
     rospy.init_node('mani_test_publisher', anonymous=False)
-    rate = rospy.Rate(0.5) # 10hz
+    rate = rospy.Rate(1)
     while not rospy.is_shutdown():
 
         msg.id      = [0,1,2,3]
         msg.speed   = [400,1424,400,1424]
-        msg.second  = [1,0.5,1,0.5]
+        msg.second  = [0.2,0.5,0.65,0.9]
 
         rospy.loginfo(msg)
         pub.publish(msg)
